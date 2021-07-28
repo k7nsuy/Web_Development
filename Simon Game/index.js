2fbbaf087e799c7b974e8acbeaaf781ada29bc32
Jordan <k7nsuy2@gmail.com>
2021-07-28 17:47:17 +0900

    jQuery & javaScript

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,8 +1,39 @@
 var buttonColors = ["red","blue","green","yellow"];
-var randomChosenColour = buttonColors[randomNumber];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  animation(userChosenColour);
+
+})
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+
+  animation(randomChosenColour);
 
 }
-nextSequence();
+
+function playSound(name) {
+  var audio = new Audio("sounds/"+name+".mp3");
+  audio.play();
+}
+
+function animation(name) {
+  $("."+name).addClass("pressed");
+   setTimeout(function() {
+       $("."+name).removeClass('pressed');
+   }, 100);
+}
